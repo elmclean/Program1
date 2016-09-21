3a8c51edf9c1187a35ec9c351a7bbfe46ded3a50
elmclean <elmclean@svsu.edu>
2016-09-21 16:16:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/elmclean/program1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.elmclean.program1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.action_main:
+                switchMain();
+                return true;
+            case R.id.action_faculty:
+                switchFaculty();
+                return true;
+            case R.id.action_courses:
+                switchCourses();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void switchMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void switchFaculty() {
+        Intent intent = new Intent(this, FacultyActivity.class);
+        startActivity(intent);
+    }
+
+    private void switchCourses() {
+        Intent intent = new Intent(this, CourseGridActivity.class);
+        startActivity(intent);
+    }
+}
